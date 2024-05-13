75eecf9ee4c2247f07c018e3839ed8aaba6ef3bc
Daniel BENET <daniel.benet@outlook.es>
2024-05-13 08:24:40 +0200

    feat: put everything together a bit more clean

--- app/screens/HabitCreationScreen.tsx
+++ app/screens/HabitCreationScreen.tsx
@@ -1,10 +1,11 @@
-import { Button, Screen, Text, TextField } from "app/components"
+import { Button, Screen, TextField } from "app/components"
 import { useStores } from "app/models"
 import { AppStackScreenProps } from "app/navigators"
 import { spacing } from "app/theme"
+import { useHeader } from "app/utils/useHeader"
 import { observer } from "mobx-react-lite"
 import React, { FC, useCallback, useState } from "react"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
 
 interface HabitCreationScreenProps extends AppStackScreenProps<"HabitCreation"> {}
 
@@ -15,6 +16,14 @@ export const HabitCreationScreen: FC<HabitCreationScreenProps> = observer(
     } = _props
     const rootStore = useStores()
     const [habit, setHabit] = useState("")
+    const [when, setWhen] = useState("")
+    const [identity, setIdentity] = useState("")
+
+    useHeader({
+      title: "New habit",
+      leftIcon: "back",
+      onLeftPress: goBack,
+    })
 
     const createHabit = useCallback(() => {
       rootStore.addHabit({ name: habit })
@@ -22,28 +31,53 @@ export const HabitCreationScreen: FC<HabitCreationScreenProps> = observer(
     }, [habit])
 
     return (
-      <Screen style={$root} preset="scroll" safeAreaEdges={["top"]}>
-        <Text preset="heading">Create a new habit</Text>
-        <TextField
-          value={habit}
-          containerStyle={$habit}
-          placeholder="Habit name"
-          onChangeText={(value) => setHabit(value)}
-        />
-        <Button disabled={!habit} preset={!habit ? "filled" : "reversed"} onPress={createHabit}>
-          Create new habit
-        </Button>
-      </Screen>
+      <>
+        <Screen
+          style={$root}
+          contentContainerStyle={$content}
+          preset="fixed"
+          safeAreaEdges={["bottom"]}
+        >
+          <View />
+          <View style={$fieldsContainer}>
+            <TextField
+              value={habit}
+              placeholder="put on my gym clothes"
+              label="I will..."
+              onChangeText={(value) => setHabit(value)}
+            />
+            <TextField
+              value={when}
+              placeholder="every morning at 7am"
+              label="When..."
+              onChangeText={(value) => setWhen(value)}
+            />
+            <TextField
+              value={identity}
+              label="So that I can become"
+              placeholder="a healthier person"
+              onChangeText={(value) => setIdentity(value)}
+            />
+          </View>
+          <Button disabled={!habit} preset={!habit ? "filled" : "reversed"} onPress={createHabit}>
+            New habit
+          </Button>
+        </Screen>
+      </>
     )
   },
 )
 
 const $root: ViewStyle = {
   flex: 1,
-  paddingHorizontal: spacing.md,
 }
 
-const $habit: ViewStyle = {
-  marginTop: spacing.md,
-  marginBottom: spacing.lg,
+const $content: ViewStyle = {
+  flex: 1,
+  paddingHorizontal: spacing.md,
+  justifyContent: "space-between",
+}
+
+const $fieldsContainer: ViewStyle = {
+  gap: spacing.md,
 }
